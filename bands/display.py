a16bfb8f5f578ab450495d8655d4e9eda9b06759
Marta Solarz <102785427+MartaSolarz@users.noreply.github.com>
2022-10-31 10:58:07 +0100

    Update display.py

--- bands/display.py
+++ bands/display.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 
-def show_bands(band) -> None:
+def show_bands(band: str) -> None:
     """
     Function plots given satellite band.
+    
+    Parameters:
+        band [str];
+    Return:
+        None.
     """
     plt.figure(figsize=(12, 12))
     plt.imshow(band, cmap='gray')
