548a6ed06d7a9ca4ba4b516806e97835cf30caea
Marta Solarz <102785427+MartaSolarz@users.noreply.github.com>
2022-11-02 21:33:13 +0100

    Update display.py

--- bands/display.py
+++ bands/display.py
@@ -1,14 +1,31 @@
-import matplotlib.pyplot as plt
+"""Funkcje wyświetlające w różny sposób obrazy satelitarne."""
 
-def show_bands(band: str) -> None:
-    """
-    Function plots given satellite band.
-    
-    Parameters:
-        band [str];
-    Return:
-        None.
-    """
+from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from bands.basic_processing import BandProcessor
+
+
+def show_bands(band: Any) -> None:
+    """Podstawowa funkcja wyświetlania obrazu satelitarnego."""
     plt.figure(figsize=(12, 12))
     plt.imshow(band, cmap='gray')
+    plt.plot()
+
+
+def show_band_index(band: Any, normalize=True, cmap='gray', cbar=False):
+    """Wyświetlanie obrazu z możliwością ustawienia skali barwnej - parametr cmap."""
+    if normalize:
+        band = (band * 255).astype(np.uint8)
+        band = BandProcessor(band).get_equalize()
+        band = band.astype(np.uint8)
+        
+    plt.figure(figsize=(12, 12))
+    plt.imshow(band, cmap=cmap)
+
+    if cbar:
+        plt.colorbar()
+    
     plt.plot()
