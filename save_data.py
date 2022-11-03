c0c5f21b8097d8a90eb34530e22763b31938a9b3
Marta Solarz <102785427+MartaSolarz@users.noreply.github.com>
2022-11-03 09:51:01 +0100

    Update save_data.py

--- save_data.py
+++ save_data.py
@@ -36,3 +36,18 @@ class SaveToTiff:
                     transform=self.transform
                     ) as dst:
             dst.write(self.band_to_save, 1)
+            
+            
+def save_resampled_image(new_band: Any, base_crs: Any, base_transform: Any, save_path: str) -> None:
+    """Zapisanie obrazu po resamplingu."""
+    with rio.open(save_path,
+                    'w',
+                    driver='GTiff',
+                    height=new_band.shape[0],
+                    width=new_band.shape[1],
+                    count=1,
+                    dtype=new_band.dtype,
+                    crs=base_crs,
+                    transform=base_transform
+                    ) as dst:
+        dst.write(new_band, 1)
