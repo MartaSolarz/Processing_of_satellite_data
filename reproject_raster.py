f49d26d83ae737d4db3875d99fd2e0026719bfb6
Marta Solarz <102785427+MartaSolarz@users.noreply.github.com>
2022-11-03 09:45:00 +0100

    Add files via upload

---
+++ reproject_raster.py
@@ -0,0 +1,14 @@
+"""Resampling obrazu."""
+
+from typing import Any
+import numpy as np
+from rasterio.warp import reproject, Resampling
+
+
+def reproject_raster(reprojected_band: Any, reprojected_transform: Any, reprojected_crs: Any, reprojecting_band: Any, reprojecting_transform: Any, reprojecting_crs: Any) -> np.ndarray:
+    """Reprojekcja obrazu rastrowego."""
+    destination = np.zeros(reprojecting_band.shape) # Tworzymy pustą tablicę na obraz po reprojekcji
+
+    reproject(reprojected_band, destination, src_transform=reprojected_transform, src_crs=reprojected_crs, dst_transform=reprojecting_transform, dst_crs=reprojecting_crs, resampling=Resampling.mode)
+    
+    return destination
